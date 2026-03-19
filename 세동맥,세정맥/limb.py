c0e378954bdab2d828a6f9445189686661bd1439
younguk <younguk072023@gmail.com>
2026-03-19 16:32:07 +0900

    marking

--- 세동맥,세정맥/limb.py
+++ 세동맥,세정맥/limb.py
@@ -1,27 +1,53 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+import os
 from skimage.morphology import skeletonize
 
-def skeletonize_image(image):
+def skeletonize_image(image,CSV_FILE):
 
     img = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
 
+    #skeletonzie
     binary_image = img > 127
-
     skeleton = skeletonize(binary_image)
+    # original + skeletonize 
+    overlay = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    overlay[skeleton] = [0,0,255]
 
-    plt.figure(figsize=(10,5))
-    plt.subplot(1,2,1)
+    #csv 검출
+    df = pd.read_csv(CSV_FILE)
+    search_name = os.path.splitext(os.path.basename(image))[0]
+
+    df['pure_name']=df['filename'].apply(lambda x: os.path.splitext(x)[0])
+
+    row = df[df['pure_name'] == search_name]
+
+    plt.figure(figsize=(12,10))
+    plt.subplot(2,2,1)
     plt.imshow(img,cmap='gray')
     plt.title("Original Image")
 
-    plt.subplot(1,2,2)
+    plt.subplot(2,2,2)
     plt.imshow(skeleton,cmap='gray')
     plt.title("Skeletonized Image")
 
+    plt.subplot(2,2,3)
+    plt.imshow(cv2.cvtColor(overlay, cv2.COLOR_BGR2RGB))
 
+    if not row.empty:
+        ux, uy = row['U_x'].values[0], row['U_y'].values[0]
+        dx, dy = row['D_x'].values[0], row['D_y'].values[0]
+
+        plt.scatter(ux, uy, c="Green", s=30)
+        plt.scatter(dx, dy, c="Yellow", s=30)
+        plt.title("Overlay with Points Image")
+
+    plt.tight_layout()
     plt.show()
 
-skeletonize_image("p9_det_019_crop_3.tif")
+CSV_FILE = "capillary_keypoint_final.csv"
+skeletonize_image("p9_det_019_crop_3.tif",CSV_FILE)
+
     
